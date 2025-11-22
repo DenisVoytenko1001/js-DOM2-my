a7bf37a1ab870ad5ede9051bb3ed47d062c16a53
Денис <voytenko501@gmail.com>
2025-11-23 01:22:59 +0200

    Add calculator project (src folder)

---
+++ src/script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+
+function append(value) {
+    if (display.innerText === "0") {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = "0";
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1) || "0";
+}
+
+function calculate() {
+    try {
+        let result = eval(display.innerText.replace('÷','/').replace('×','*'));
+        display.innerText = result;
+    } catch {
+        display.innerText = "Error";
+    }
+}
